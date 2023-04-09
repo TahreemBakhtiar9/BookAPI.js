55fa8f7b33818dac061d24f51dfa6e446e259759
TahreemBakhtiar9 <tehreembakhtiar927@gmail.com>
2023-04-09 22:27:21 +0500

    add book and return list test pass

--- Tests/first.test.js
+++ Tests/first.test.js
@@ -91,3 +91,127 @@ test ('Remove book from list', ()=> {
     //assert
     expect(result).toStrictEqual(assert)
 })
+
+// ------------------------------------------------------------------
+//TEST 05
+
+test('Add a new book and return all books from the list', () => {
+    //arrange
+    let assert = [
+        {
+            title: "It ends with us",
+            author: "Colleen Hoover ",
+            length: 389,
+            year: 2017,
+            ReadingDate: "Dec, 2023",
+            rating: 3
+        },
+
+        {
+            title: "The Hobbit",
+            author: "J. R. R Tolkein ",
+            length: 1058,
+            year: 2012,
+            ReadingDate: "Sept, 2014",
+            rating: 5 
+        },
+
+        {
+            title: "A Dance With Dragons",
+            author: "George R. R. Martin",
+            length: 687,
+            year: 2011,
+            ReadingDate: "April, 2015",
+            rating: 5   
+        }
+    ]
+
+    const Book = {
+        
+            title: "A Dance With Dragons",
+            author: "George R. R. Martin",
+            length: 687,
+            year: 2011
+    }
+    const ReadingDate = "April, 2015";
+    const rating = 5; 
+
+    //act
+    addBook(Book , ReadingDate, rating);
+    let result = getBook();
+
+    //assert
+    expect(result).toStrictEqual(assert)
+})
+
+
+
+
+// test("add the new book and return all list of book", () => {
+//     // Arrange
+//     let assert = [
+//       {
+//         Author: "jhon",
+//         ReadingDate: "2039/jan",
+//         length: 253,
+//         rating: 3,
+//         title: "the alpha",
+//         year: 2020,
+//       },
+//       {
+//         Author: "jhon",
+//         ReadingDate: "2000/jan",
+//         length: 253,
+//         rating: 4,
+//         title: "book",
+//         year: 2020,
+//       },
+//       {
+//         Author: "jhon",
+//         ReadingDate: "2039/jan",
+//         length: 253,
+//         rating: 4,
+//         title: "the beta",
+//         year: 2020,
+//       },
+//     ];
+  
+//     const Book = {
+//       title: "the beta",
+//       Author: "jhon",
+//       length: 253,
+//       year: 2020,
+//     };
+  
+//     const ReadingDate = "2039/jan";
+//     const rating = 4;
+  
+//     addBook(Book, ReadingDate, rating);
+  
+//     //  Arrang
+//     let result = getbook();
+//     // Assert
+//     expect(result).toEqual(assert);
+//   });
+  
+//   test("get book by rating ", () => {
+//        // Arrange
+//     const assert = [
+//       {
+//         Author: "jhon",
+//         ReadingDate: "2039/jan",
+//         length: 253,
+//         rating: 3,
+//         title: "the alpha",
+//         year: 2020,
+//       },
+//     ];
+//     let rating = 3;
+//           //Act
+//     getBooksByRating(rating);
+  
+//     const result = getbook();
+//         // Assert
+//     expect(result).toEqual(assert);
+//   });
+  
